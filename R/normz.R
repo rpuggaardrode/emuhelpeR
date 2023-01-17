47ea3e8acac2f714ef2329dc3170ea947c956afd
Rasmus Puggaard-Rode <r.puggaard@phonetik.uni-muenchen.de>
2023-01-17 16:53:46 +0100

    Initial

--- R/normz.R
+++ R/normz.R
@@ -1,3 +1,28 @@
+#' Normalize by speaker and rescale
+#'
+#' Performs by-speaker z-score normalization on a given variable and stores it
+#' in a separate column, then rescales the z-scores based on the overall mean
+#' and SD values in the data.
+#'
+#' @param df A data frame containing a column with the variable to be normalized
+#' and rescaled, and optionally a column containing a unique id for each speaker.
+#' @param var A string giving the name of the column in `df` containing the
+#' continuous variable to be normalized.
+#' @param speaker An optional string giving the name of the column in `df`
+#' containing speakers id's. Default is `NULL`.
+#'
+#' @return A data frame identical to `df` with the added columns `z{var}` and
+#' `norm{var}`.
+#' @export
+#'
+#' @examples
+#' colnames(ssff_data)
+#' unique(ssff_data$speaker)
+#' head(ssff_data$eggF0, 20)
+#' x <- normz(df=ssff_data, var='eggF0', speaker='speaker')
+#' colnames(x)
+#' head(x$zeggF0, 20)
+#' head(x$normeggF0, 20)
 normz <- function(df, var, speaker=NULL) {
 
   if(!is.null(speaker)) {
