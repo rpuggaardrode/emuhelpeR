1a1a5830fbc463f1ee1a3aee21e194328e874fbb
Rasmus Puggaard-Rode <r.puggaard@phonetik.uni-muenchen.de>
2023-02-27 11:55:06 +0100

    Added convenience functions for setting canvas order

---
+++ R/add_signal_canvas.R
@@ -0,0 +1,26 @@
+#' Add signal canvases to EMU database
+#'
+#' Wrapper to add one or more signal canvases to the default view of an EMU
+#' database.
+#'
+#' @param db_handle The handle of an EMU database which is already loaded in R.
+#' @param add One or more strings containing the names of canvases to add to
+#' the EMU database.
+#'
+#' @seealso This function wraps the functions [emuR::get_signalCanvasesOrder]
+#' and [emuR::set_signalCanvasesOrder].
+#' @export
+#'
+#' @examples
+#' datapath <- system.file('extdata/db', package='emuhelpeR')
+#' raw <- emuR::load_emuDB(datapath)
+#' emuR::get_signalCanvasesOrder(raw, 'default')
+#' add_signal_canvas(db_handle=raw, add='eggF0')
+#' emuR::get_signalCanvasesOrder(raw, 'default')
+#' #To have a look at the new order in EMU-SDMS, run
+#' #emuR::serve(raw)
+add_signal_canvas <- function(db_handle, add) {
+  sco <- emuR::get_signalCanvasesOrder(db_handle, 'default')
+  emuR::set_signalCanvasesOrder(db_handle, 'default', c(sco, add))
+}
+
---
+++ R/reset_signal_canvas.R
@@ -0,0 +1,23 @@
+#' Reset signal canvas order in EMU database
+#'
+#' Sets the default signal canvas order in an EMU database to the standard
+#' setting of waveform and spectrogram.
+#'
+#' @param db_handle The handle of an EMU database which is already loaded in R.
+#'
+#' @seealso Wraps the function [emuR::set_signalCanvasesOrder].
+#' @export
+#'
+#' @examples
+#' datapath <- system.file('extdata/db', package='emuhelpeR')
+#' raw <- emuR::load_emuDB(datapath)
+#' emuR::get_signalCanvasesOrder(raw, 'default')
+#' add_signal_canvas(db_handle=raw, add='eggF0')
+#' emuR::get_signalCanvasesOrder(raw, 'default')
+#' reset_signal_canvas(db_handle=raw)
+#' emuR::get_signalCanvasesOrder(raw, 'default')
+reset_signal_canvas <- function(db_handle) {
+  sco <- c('OSCI', 'SPEC')
+  emuR::set_signalCanvasesOrder(db_handle, 'default', sco)
+}
+
