469d040401866188900e03c4315cce6c121d3896
Rasmus Puggaard-Rode <r.puggaard@phonetik.uni-muenchen.de>
2023-04-13 11:19:40 +0200

    Updated load_db()

--- R/load_db.R
+++ R/load_db.R
@@ -1,22 +1,24 @@
-#' Load EMU-SDMS database object
+#' Load and serve EMU-SDMS database object
 #'
-#' Convenience function for loading an EMU-SDMS database without specifying
+#' Convenience function for loading and serving EMU-SDMS database in a single
+#' command without specifying
 #' its name whenever there is only one EMU-SDMS database in the working
 #' directory and its subdirectories.
 #'
 #' @param recursive Logical; default is `TRUE`. Whether or not to recursively
 #' search subdirectories of `dir` for
 #' EMU databases.
+#' @param serve Logical; whether or not to serve the database in a browser window.
 #'
 #' @return A list containing an EMU-SDMS database object.
-#' @seealso This is a wrapper for the function [emuR::load_emuDB()].
+#' @seealso This is a wrapper for the functions [emuR::load_emuDB()] and
+#' [emuR::serve()].
 #' @export
 #'
 #' @examples
 #' # don't run
 #' # x <- load_db()
-#' # serve()
-load_db <- function(recursive=TRUE) {
+load_db <- function(recursive=TRUE, serve=TRUE) {
 
   dirs <- list.dirs(full.names=F, recursive=recursive)
   emuDB <- which(stringr::str_sub(dirs, start=-5) == 'emuDB')
@@ -26,6 +28,11 @@ load_db <- function(recursive=TRUE) {
                 'Please specify a directory with only one emuDB.'))
   }
 
-  emuR::load_emuDB(dirs[emuDB])
+  if (serve) {
+    emuR::serve(emuR::load_emuDB(dirs[emuDB]))
+  } else {
+    emuR::load_emuDB(dirs[emuDB])
+  }
 
 }
+
